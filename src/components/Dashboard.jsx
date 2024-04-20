39d4920acd68ad902dd89c3c47ef77d59eceef64
zeenat <zeenu.23@gmail.com>
2024-04-20 11:48:15 +0530

    add @ant-design/icons to display icons in Dashboard sidebar menu

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,7 @@
-import {Layout, Menu, theme } from 'antd'
+import { Layout, Menu, theme } from 'antd'
+import { DollarCircleOutlined, AccountBookOutlined, BarChartOutlined } from '@ant-design/icons';
 const { Sider } = Layout;
+const { SubMenu } = Menu;
 
 function Dashboard() {
     const {
@@ -18,11 +20,19 @@ function Dashboard() {
         mode="inline"
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
-        style={{
-          height: '100%',
-        }}
-        items=""
-      />
+        style={{ height: '100%', borderRight: 0 }}
+      >
+        <SubMenu key="sub1" icon={<DollarCircleOutlined />} title="Bills & Payments">
+          <Menu.Item key="1">Bill Management</Menu.Item>
+          <Menu.Item key="2">Payment History</Menu.Item>
+        </SubMenu>
+        <Menu.Item key="3" icon={<AccountBookOutlined />}>
+          Expenses
+        </Menu.Item>
+        <Menu.Item key="4" icon={<BarChartOutlined />}>
+          My Stats
+        </Menu.Item>
+      </Menu>
       </Sider>
     </Layout>
   )
