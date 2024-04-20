6291ec7d0b86bb4d3348c28b7dff9325c8975acf
zeenat <zeenu.23@gmail.com>
2024-04-20 15:55:48 +0530

    replace Divider with Ant Design Sider and Menu components to display transaction history and Burger King

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { Divider } from 'antd'
+import { Layout, Menu } from 'antd'
+const { Sider } = Layout;
 
 function TransactionHistory() {
   return (
-      <div>
-      <h1>Your Transaction History</h1>
-      <Divider />
-      <h1>Burger King</h1>
-      </div>
+    <Sider
+        width={300}
+        theme="dark"  
+        style={{ height: '100vh' }}     
+    >
+      
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{ height: '100%', borderRight: 0 }}
+        theme="dark"
+      >
+        <Menu.Item key="3" >
+           Your Transaction History
+        </Menu.Item>
+        <Menu.Item key="4" >
+          Burger King
+        </Menu.Item>
+      </Menu>
+     </Sider>
+      
   )
 }
 
